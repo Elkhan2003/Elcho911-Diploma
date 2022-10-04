6c9a644282ee7a51e917f0832f15595b8f9e1b9a
Elkhan <98739225+Elkhan2003@users.noreply.github.com>
2022-10-04 17:30:34 +0600

    Update to loader

--- src/Pages/Compiler.jsx
+++ src/Pages/Compiler.jsx
@@ -1,38 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import { Triangle } from 'react-loader-spinner'
-import { Container } from 'react-bootstrap'
-import css from './Pages.module.css'
+import React, { useState, useEffect } from "react";
+import { Triangle } from "react-loader-spinner";
+import { Container } from "react-bootstrap";
+import css from "./Pages.module.css";
 
 export const Compiler = () => {
+	const [site, setSite] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false)
+	useEffect(() => {
+		setLoading(true);
+		fetch("https://my-json-server.typicode.com/Elkhan2003/backend/sites")
+			.then((res) => res.json())
+			.then((json) => {
+				setSite(json);
+				setLoading(false);
+			});
+	}, []);
 
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 1000)
-    }, [])
-
-    return (
-        <div className={css.content}>
-            <Container>
-                {
-                    loading ?
-                        <div className={css.loading}>
-                            <Triangle
-                                height="200"
-                                width="200"
-                                color='red'
-                                ariaLabel='loading'
-                            />
-                        </div>
-                        :
-                        <div className={css.sites__url}>
-                            <iframe className={css.site__url} src="https://rextester.com/l/pascal" scrolling="auto"> </iframe>
-                        </div>
-                }
-            </Container>
-        </div>
-    )
-}
+	return (
+		<div className={css.content}>
+			<Container>
+				{loading && (
+					<div className={css.loading}>
+						<Triangle
+							height="200"
+							width="200"
+							color="red"
+							ariaLabel="loading"
+						/>
+					</div>
+				)}
+				{site.map((el) => {
+					return (
+						<div className={css.sites__url}>
+							<iframe className={css.site__url} src={el.url1}></iframe>
+						</div>
+					);
+				})}
+			</Container>
+		</div>
+	);
+};
--- src/Pages/Map.jsx
+++ src/Pages/Map.jsx
@@ -1,38 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { Triangle } from 'react-loader-spinner'
-import { Container } from 'react-bootstrap'
-import css from './Pages.module.css'
+import React, { useState, useEffect } from "react";
+import { Triangle } from "react-loader-spinner";
+import { Container } from "react-bootstrap";
+import css from "./Pages.module.css";
 
 export const Map = () => {
+	const [site, setSite] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false)
+	useEffect(() => {
+		setLoading(true);
+		fetch("https://my-json-server.typicode.com/Elkhan2003/backend/maps")
+			.then((response) => response.json())
+			.then((json) => {
+				setSite(json);
+				setLoading(false);
+			});
+	},[]);
 
-    useEffect(() => {
-        setLoading(true)
-        setTimeout(() => {
-            setLoading(false)
-        }, 1000)
-    }, [])
+	return (
+		<div className={css.content}>
+			<Container>
+				{loading && (
+					<div className={css.loading}>
+						<Triangle
+							height="200"
+							width="200"
+							color="red"
+							ariaLabel="loading"
+						/>
+					</div>
+				)}
 
-    return (
-        <div className={css.content}>
-            <Container>
-                {
-                    loading ?
-                        <div className={css.loading}>
-                            <Triangle
-                                height="200"
-                                width="200"
-                                color='red'
-                                ariaLabel='loading'
-                            />
-                        </div>
-                        :
-                        <div className={css.sites__url}>
-                            <iframe className={css.site__url} src="https://elkhan2003.github.io/elcho_map/" scrolling="auto"> </iframe>
-                        </div>
-                }
-            </Container>
-        </div>
-    )
-}
+				{site.map((el) => {
+					return (
+						<>
+							<div className={css.sites__url}>
+								<iframe className={css.site__url} src={el.url1}></iframe>
+							</div>
+						</>
+					);
+				})}
+			</Container>
+		</div>
+	);
+};
