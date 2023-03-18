7d1ff6b3da1becb3b7443798f4ec9f8e6381e615
Elcho911 <98739225+Elkhan2003@users.noreply.github.com>
2023-03-19 01:21:43 +0600

    Update Wrapper.jsx

--- src/Components/wrapper/Wrapper.jsx
+++ src/Components/wrapper/Wrapper.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Wrapper.scss";
-import { Header } from "./Header/Header.jsx";
+import { Header } from "./header/Header.jsx";
 import { Main } from "./main/Main.jsx";
 import { Footer } from "./footer/Footer.jsx";
 
